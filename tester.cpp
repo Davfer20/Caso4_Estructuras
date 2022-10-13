2ac355e01c04ea69c664c42370eaa3fbc86fdcec
XoulEater <99497779+XoulEater@users.noreply.github.com>
2022-10-12 20:58:37 -0600

    Generación completa

--- tester.cpp
+++ tester.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    
-    Mina* mina1 = new Mina();
-    mina1->addToSalap();
 
+    Mina *mina1 = new Mina();
+    mina1->addToSalap();
+    mina1->addSalasTotal();
 
     return 0;
 }
