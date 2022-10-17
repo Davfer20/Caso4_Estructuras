d2e1a4defe6b57db5a96180a7422eedf7c499506
XoulEater <99497779+XoulEater@users.noreply.github.com>
2022-10-17 17:58:18 -0600

    Interfaz v1

--- Miner.h
+++ Miner.h
@@ -9,80 +9,100 @@
 
 #define MINER 1
 
-using namespace std; 
+using namespace std;
 
-struct timespec sec1 = {0, 5}; // Pausa de un segundo 
+struct timespec sec1 = {1, 0}; // Pausa de un segundo
 
 class Miner
 {
-    private:
-        Sala* currentSala;
-        vector<int> disPaths;
-        string action;
-        string Mname;
-        Mina* currentMina;
-        int Mtype; // 0 - Explorer, 1 - Carrier, 2 - Driller
-        int Mstrat;
-        int inventory;
-        int cap;
-        int speed;
-        int lastDir;
+private:
+    Sala *currentSala;
+    vector<int> disPaths;
+    string action;
+    string Mname;
+    Mina *currentMina;
+    int Mtype; // 0 - Explorer, 1 - Carrier, 2 - Driller
+    int Mstrat;
+    int inventory;
+    int cap;
+    int speed;
+    int lastDir;
+    int state; // 0 sala, 1 tunel
 
+public:
+    Miner(int PmType, int PmStrat, Mina *Pmina)
+    {
+        this->currentMina = Pmina;
+        this->currentSala = Pmina->getSalasList()->find(0);
+        this->inventory = 0;
+        this->Mstrat = PmStrat;
+        switch (PmType)
+        {
+        case 0:
+            this->Mtype = 0;
+            this->Mname = "Explorer";
+            this->speed = 20;
+            this->cap = 8;
+            break;
+        case 1:
+            this->Mtype = 1;
+            this->Mname = "Carrier";
+            this->speed = 10;
+            this->cap = 40;
+            break;
 
-    public:
-        Miner(int PmType, int PmStrat, Mina* Pmina) { 
-            this->currentMina = Pmina;
-            this->currentSala = Pmina->getSalasList()->find(0);
-
-            this->Mstrat = PmStrat;
-            switch (PmType)
-            {
-            case 0:
-                this->Mtype = 0;
-                this->Mname = "Explorer";
-                this->speed = 20;
-                this->cap = 8;
-                break;
-            case 1:
-                this->Mtype = 1;
-                this->Mname = "Carrier";
-                this->speed = 10;
-                this->cap = 40;
-                break;
-            
-            default:
-                this->Mtype = 2;
-                this->Mname = "Driller";
-                this->speed = 7;
-                this->cap = 15;
-                break;
-            }
-        }
-        string getMname() {
-            return this->Mname;
+        default:
+            this->Mtype = 2;
+            this->Mname = "Driller";
+            this->speed = 7;
+            this->cap = 15;
+            break;
         }
 
-        void mining() {
-            srand(this->Mtype);
-            while (true) {
-                pthread_delay_np(&sec1);
-                cout << "El " << this->getMname() << " está en la sala " << currentSala->getID() << endl;
-                if (currentSala->getIfTunel() && !(rand() % 3))
-                {
-                    break;   
-                }
-                
-                disPaths = currentSala->availablePaths();
-                int rDire;
-                int max = disPaths.size();  
-                rDire = rand();
-                rDire %= max;  
-                rDire = disPaths[rDire];
-                this->currentSala = currentSala->getSalaDir(rDire);
-            }
-            cout << "El " << this->getMname() << " entró al tunel de la sala " << currentSala->getID() << endl; 
-        }
+    }
+    string getMname()
+    {
+        return this->Mname;
+    }
 
+    string getAction()
+    {
+        return this->action;
+    }
+
+    int getInventory()
+    {
+        return this->inventory;
+    }
+
+    int getState()
+    {
+        return this->state;
+    }
+
+    Sala* getSala()
+    {
+        return this->currentSala;
+    }
+
+    void mining()
+    {
+        this->action = "Explorando la sala " + to_string(currentSala->getID());
+        if (currentSala->getIfTunel() && !(rand() % 3))
+        {
+            this->action = "Explorando el tunel " + to_string(currentSala->getID());
+            this->state = 1;
+        } else {
+            disPaths = currentSala->availablePaths();
+            int rDire;
+            int max = disPaths.size();
+            rDire = rand();
+            rDire %= max;
+            rDire = disPaths[rDire];
+            this->currentSala = currentSala->getSalaDir(rDire);
+        }
+        
+    }
 };
 
 #endif
