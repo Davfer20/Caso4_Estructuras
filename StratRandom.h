48deef4b67c21d8253590ed086a999f43e593024
XoulEater <99497779+XoulEater@users.noreply.github.com>
2022-10-23 12:12:06 -0600

    New IA Proyect

---
+++ StratRandom.h
@@ -0,0 +1,24 @@
+#include "IStrat.h"
+
+class StratRandom : public IStrat
+{
+private:
+    /* Atributos */
+public:
+    StratRandom()
+    {
+    }
+    int chooseSala(vector<int> AvailPaths)
+    {
+    }
+    bool enterTunel()
+    {
+    }
+    bool mineOrgGodeep()
+    {
+    }
+    bool choosePath() = 0;
+    string getName()
+    {
+    }
+};
